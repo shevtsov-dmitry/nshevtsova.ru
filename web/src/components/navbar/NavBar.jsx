858ffac026961803636b989727f871843e8c34d4
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-07-16 18:09:51 +0300

    refactor:  add css file for navbar

--- web/src/components/navbar/NavBar.jsx
+++ web/src/components/navbar/NavBar.jsx
@@ -1,3 +1,27 @@
 export default function NavBar() {
-  return <div className="absolute h-10 w-10 "></div>
+    return <div className="z-10 w-full bg-red-300 h-[5rem] flex justify-between items-center">
+        {/* ⮟   ⯆  */}
+        <ul id="navBarTabs" className="flex gap-2">
+            <li className="navBarTab">Портфолио</li>
+            <li className="navBarTab">отзывы</li>
+            <li className="navBarTab">
+                цены <span className="navTabArrowDown">⯆</span>
+            </li>
+            <li className="navBarTab">
+                услуги<span className="navTabArrowDown">⯆</span>
+            </li>
+            <li className="navBarTab">новости</li>
+        </ul>
+
+        <div id="phone-number-and-icons" className="flex">
+            <p>+7 (903) 733-57-57</p>
+            <ul id="icons" className="flex">
+                <img src="images/navbar/telegram.png" className="socialMediaIcon" />
+                <img src="images/navbar/viber.png" className="socialMediaIcon" />
+                <img src="images/navbar/vk.png" className="socialMediaIcon" />
+                <img src="images/navbar/whatsapp.png" className="socialMediaIcon" />
+            </ul>
+        </div>
+
+    </div >
 }

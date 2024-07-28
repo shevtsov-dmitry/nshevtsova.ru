e83f17100ad656c768c2639a8c22d3969ecf3190
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-07-28 12:33:52 +0300

    fix: remove temporary transefered object

--- joes/src/App.jsx
+++
@@ -1,8 +0,0 @@
-function App() {
-    return <div className={'w-full h-fit flex flex-col justify-center items-center'}>
-        <img className={"w-[40%]"} src={'nobj.jpg'} />
-        <img src={'resignation.jpg'} />
-    </div>
-}
-
-export default App

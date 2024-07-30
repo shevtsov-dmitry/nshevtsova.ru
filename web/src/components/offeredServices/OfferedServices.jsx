a28e8f304496cf34feef38bfae324e19ac86560a
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-07-30 10:50:14 +0300

    feat: add offered services assets, write description

--- web/src/components/offeredServices/OfferedServices.jsx
+++ web/src/components/offeredServices/OfferedServices.jsx
@@ -1,3 +1,63 @@
+import ServiceBlock from './ServiceBlock'
+
 export default function OfferedServices() {
-  return <div className="h-screen w-full bg-orange-200"></div>
+  return (
+    <section className="grid h-screen w-screen grid-cols-3 gap-5 p-5">
+      <ServiceBlock
+        title={'ПОКУПКА'}
+        icon={'images/offered-services/house-key.png'}
+        description={[
+          'Подбор идеальной недвижимости',
+          'Сопровождение сделки на каждом этапе',
+          'Юридическая проверка документов',
+          'Переговоры с продавцами',
+        ]}
+      />
+      <ServiceBlock
+        title={'ПРОДАЖА'}
+        icon={'images/offered-services/house.png'}
+        description={[
+          'Оценка рыночной стоимости',
+          'Подготовка к продаже',
+          'Маркетинг и реклама',
+          'Проведение показов',
+        ]}
+      />
+      <ServiceBlock
+        title={'ПОИСК НЕДВИЖИМОСТИ'}
+        icon={'images/offered-services/find-house1.png'}
+        description={[
+          'Поиск по заданным критериям',
+          'Консультации по районам',
+          'Организация просмотров',
+          'Анализ рынка недвижимости',
+        ]}
+      />
+      <ServiceBlock
+        title={'АРЕНДА'}
+        icon={'images/offered-services/rent1.png'}
+        description={[
+          'Поиск долгосрочной аренды',
+          'Оформление договоров',
+          'Консультации по аренде',
+          'Подбор арендаторов',
+        ]}
+      />
+      <ServiceBlock
+        title={'ПОМОЩЬ В ОФОРМЛЕНИИ'}
+        icon={'images/offered-services/document.png'}
+        description={[
+          'Сбор необходимых документов',
+          'Юридическое сопровождение',
+          'Оформление ипотеки',
+          'Консультации по налогам',
+        ]}
+      />
+      <ServiceBlock
+        title={'КОНСУЛЬТАЦИЯ'}
+        icon={'images/offered-services/consult.png'}
+        description={[]}
+      />
+    </section>
+  )
 }
---
+++ web/src/components/offeredServices/ServiceBlock.jsx
@@ -0,0 +1,5 @@
+export default function ServiceBlock() {
+  return (
+    <div className="h-full w-full rounded-[0.75rem] border border-gray-300"></div>
+  )
+}

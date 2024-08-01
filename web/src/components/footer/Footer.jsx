aff0540e5ee7e3620fe487b47af601a174d865c9
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-08-01 17:10:40 +0300

    feat: add new icons to footer

--- web/src/components/footer/Footer.jsx
+++ web/src/components/footer/Footer.jsx
@@ -1,5 +1,8 @@
 export default function Footer() {
     return <footer className="w-full h-96 bg-neutral-900">
+        <div id="phone-number" className="flex">
+            <img src="images/footer/phone.png" />
 
+        </div>
     </footer>
 }

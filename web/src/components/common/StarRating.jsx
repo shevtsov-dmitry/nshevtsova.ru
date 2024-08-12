19958f4daec38447d51e59f56dbddb90666940fe
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-08-12 15:29:40 +0300

    feat: add reviews switcher

--- web/src/components/common/StarRating.jsx
+++ web/src/components/common/StarRating.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { FaStar } from 'react-icons/fa';
 
-export default function StarRating( {stars, isDefaultChecked}) {
+export default function StarRating({ stars, isDefaultChecked }) {
     const [rating, setRating] = useState(0);
     const [hover, setHover] = useState(0);
 
-    const STAR_SIZE = 30
+    const STAR_SIZE = 30;
 
     return (
         <div className="flex">
@@ -21,33 +21,33 @@ export default function StarRating( {stars, isDefaultChecked}) {
                             // defaultChecked={rating === ratingValue}
                             defaultChecked={true}
                             onClick={() => {
-                              if (isDefaultChecked) {
-                                return
-                              }
-                              setRating(ratingValue);
+                                if (isDefaultChecked) {
+                                    return;
+                                }
+                                setRating(ratingValue);
                             }}
                         />
-                          {isDefaultChecked ?
-                          <FaStar
-                              size={STAR_SIZE}
-                              className={` ${
-                                  ratingValue <= stars
-                                      ? 'text-yellow-500'
-                                      : 'text-gray-300'
-                              }`}
-                          />
-                          :
-                          <FaStar
-                              size={STAR_SIZE}
-                              className={`cursor-pointer transition-colors duration-200 ${
-                                  ratingValue <= (hover || rating)
-                                      ? 'text-yellow-500'
-                                      : 'text-gray-300'
-                              }`}
-                              onMouseEnter={() => setHover(ratingValue)}
-                              onMouseLeave={() => setHover(0)}
-                          />
-                        }
+                        {isDefaultChecked ? (
+                            <FaStar
+                                size={STAR_SIZE}
+                                className={` ${
+                                    ratingValue <= stars
+                                        ? 'text-yellow-500'
+                                        : 'text-gray-300'
+                                }`}
+                            />
+                        ) : (
+                            <FaStar
+                                size={STAR_SIZE}
+                                className={`cursor-pointer transition-colors duration-200 ${
+                                    ratingValue <= (hover || rating)
+                                        ? 'text-yellow-500'
+                                        : 'text-gray-300'
+                                }`}
+                                onMouseEnter={() => setHover(ratingValue)}
+                                onMouseLeave={() => setHover(0)}
+                            />
+                        )}
                     </label>
                 );
             })}

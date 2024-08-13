e2c179df15aae8454657edac5c4a91c8a0827923
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-08-13 10:29:54 +0300

    feat: smooth reviews switch animation

--- web/src/components/reviews/Reviews.jsx
+++ web/src/components/reviews/Reviews.jsx
@@ -17,15 +17,41 @@ export default function Reviews() {
         defaultUserData
     ]);
 
-    const [midReviewIdx, setMidReviewIdx] = useState(1);
+    const [midIdx, setMidIdx] = useState(1);
 
     const positions = {
-        LEFT: midReviewIdx - 1,
-        MID: midReviewIdx,
-        RIGHT: midReviewIdx + 1
+        LEFT: midIdx - 1,
+        MID: midIdx,
+        RIGHT: midIdx + 1
     };
 
+    const sliderRef = useRef();
+
     const GLOBAL_VALUES = useSelector((state) => state.globalStringValues);
+
+    let scrollDistancePx = 300;
+
+    if (sliderRef.current) {
+        scrollDistancePx = sliderRef.current.offsetWidth / 3;
+    }
+
+    function switchToPrev() {
+        setMidIdx(midIdx - 1);
+        // setIsScrollRight(false);
+        if (sliderRef.current) {
+            sliderRef.current.scrollLeft -= scrollDistancePx;
+        }
+    }
+
+    function switchToNext() {
+        setMidIdx(midIdx + 1);
+        // setIsScrollRight(true);
+        if (sliderRef.current) {
+          console.log("before",sliderRef.current.scrollLeft)
+            sliderRef.current.scrollLeft += scrollDistancePx;
+            console.log("after",sliderRef.current.scrollLeft)
+        }
+    }
 
     useEffect(() => {
         async function fetchUserReviews() {
@@ -40,26 +66,19 @@ export default function Reviews() {
 
     function ReviewDiv({ positionIdx, json }) {
         const [isShowMore, setIsShowMore] = useState(false);
-
-        const isMid = positionIdx === positions.MID;
-
-        const [divHeight, setDivHeight] = useState('h-auto');
+        const midElRef = useRef()
 
         useEffect(() => {
-            let height = '';
-            if (isMid) {
-                height = isShowMore ? 'h-auto' : 'h-80';
-            } else {
-                height = isShowMore ? 'h-auto' : 'h-52';
-                console.log(height);
-            }
-            setDivHeight(height);
-        }, [isShowMore]);
+          if (positionIdx === positions.MID) {
+            midElRef.current.style.transform = "scale(1.25)"
+            midElRef.current.classList.add("animate-scale-up")
+          }
+        }, [midIdx])
 
         return (
             <div
-                // className={`flex w-full flex-col gap-3 rounded bg-white px-[1.5%] py-[1%] ${isMid ? 'h-80' : 'h-52'} ${isShowMore ? 'h-[1000px]' : 'h-[20px]'}`}
-                className={`${divHeight} ${isShowMore && 'max-h-92'} flex w-full flex-col gap-3 rounded bg-white px-[1.5%] py-[1%]`}
+                ref={midElRef}
+                className={`mx-[1.5%] h-[20em] w-[30%] flex-shrink-0 flex flex-col p-5 bg-white rounded-lg`}
                 style={{
                     boxShadow: 'rgba(0, 0, 0, 0.56) 0px 22px 70px 4px'
                 }}
@@ -82,18 +101,41 @@ export default function Reviews() {
                 </div>
                 <p className="overflow-hidden">{json.reviewText}</p>
                 <p
-                    className={`w-fit font-[0.7rem] underline hover:cursor-pointer hover:text-blue-500`}
+                    className={`w-fit select-none font-[0.7rem] underline hover:cursor-pointer hover:text-blue-500 py-[5%]`}
                     onClick={() => setIsShowMore(isShowMore ? false : true)}
                 >
-                    {isShowMore ? 'закрыть' : 'посмотреть полностью'}
+                  {isShowMore ?  'закрыть' : 'посмотреть полностью' }
                 </p>
             </div>
         );
     }
 
+    function SwitchArrows () {
+
+      return (
+      <div className="flex w-full justify-center">
+          <div className="flex items-center gap-5 z-50 bg-pink-400">
+              <FaArrowLeft
+                  size={30}
+                  className="switch-review-arrow"
+                  onClick={() => switchToPrev()}
+              />
+              <p className="select-none text-[2rem] font-bold text-white">
+                  1
+              </p>
+              <FaArrowRight
+                  size={30}
+                  className="switch-review-arrow"
+                  onClick={ () => switchToNext()}
+              />
+          </div>
+      </div>
+      )
+    }
+
     return (
         <div
-            className={`flex h-full flex-col gap-24 bg-[url('images/reviews/foggy-city.jpg')] bg-center bg-no-repeat px-[6%] py-[2%]`}
+            className={`flex h-full flex-col bg-[url('images/reviews/foggy-city.jpg')] bg-center bg-no-repeat py-[2%]`}
         >
             <div className="h-full w-full flex-1">
                 <h1 className="text-center font-ptsans-bold text-4xl">
@@ -101,38 +143,20 @@ export default function Reviews() {
                     помощью
                 </h1>
             </div>
-            <div className="flex-2 flex w-full flex-col items-center">
-                <div className="flex items-center gap-5 max-mobile:flex-col">
-                    <ReviewDiv
-                        positionIdx={positions.LEFT}
-                        json={reviewsJsonArray[positions.LEFT]}
-                    />
-                    <ReviewDiv
-                        positionIdx={positions.MID}
-                        json={reviewsJsonArray[positions.MID]}
-                    />
-                    <ReviewDiv
-                        positionIdx={positions.RIGHT}
-                        json={reviewsJsonArray[positions.RIGHT]}
-                    />
+            <div className="flex-2 flex h-full w-full flex-col items-center justify-center">
+                <div
+                    ref={sliderRef}
+                    className="flex z-20 max-mobile:h-full max-mobile:flex-col items-center w-full scroll-smooth overflow-hidden py-[10%]"
+                >
+                    {reviewsJsonArray.map((json, idx) => (
+                        <ReviewDiv key={idx} json={json} positionIdx={idx} />
+                    ))}
                 </div>
-                <div>
-                    <div className="absolute flex items-center gap-5">
-                        <FaArrowLeft
-                            size={30}
-                            className="switch-review-arrow"
-                        />
-                        <p className="text-[2rem] font-bold text-white">0</p>
-                        <FaArrowRight
-                            size={30}
-                            className="switch-review-arrow"
-                        />
-                    </div>
-                </div>
+                  <SwitchArrows />
             </div>
             <div className="flex w-full flex-1 justify-center">
                 <button
-                    className="w-fit rounded-lg bg-white px-5 pb-4 pt-3 font-ptsans-bold text-3xl transition-all hover:scale-105"
+                    className="w-fit select-none rounded-lg bg-white px-5 pb-4 pt-3 font-ptsans-bold text-3xl transition-all hover:scale-105"
                     style={{
                         boxShadow:
                             'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset'

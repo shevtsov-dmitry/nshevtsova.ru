c81788ef73825a3d9b7ae1d0e3704eb22fa6caa1
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-08-14 12:19:16 +0300

    feat: add burger to social medias in laptop view

--- web/src/components/navbar/Navbar.jsx
+++ web/src/components/navbar/Navbar.jsx
@@ -1,54 +1,87 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Navigation from './Navigation';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FaBars, FaWindowClose } from 'react-icons/fa';
 
 export default function Navbar() {
-    const [heightCSS, setHeightCSS] = useState('h-12');
     const GLOBAL_VALUES = useSelector((state) => state.globalStringValues);
+    const [heightCSS, setHeightCSS] = useState('h-12');
+    const [isBurgerOpened, setIsBurgerOpened] = useState(false);
+
+    function SocialMediaIcons() {
+        return (
+            <div
+                id="icons-holder"
+                className={`${heightCSS} ${isBurgerOpened ? 'max-laptop:flex' : 'max-laptop:hidden'} flex w-fit gap-7 max-laptop:flex-col max-laptop:gap-5`}
+            >
+                <img
+                    src="images/navbar/telegram.png"
+                    className="icon"
+                    alt="telegram"
+                />
+                <img
+                    src="images/navbar/viber.png"
+                    className="icon"
+                    alt="viber"
+                />
+                <img src="images/navbar/vk.png" className="icon" alt="vk" />
+                <img
+                    src="images/navbar/whatsapp.png"
+                    className="icon"
+                    alt="whatsapp"
+                />
+            </div>
+        );
+    }
+
+    function PhoneNumber() {
+        return (
+            <div id="phone-number-holder" className="mb-[-3px]">
+                <u className="phone-number-ul">
+                    <p
+                        className="phone-number-p text-nowrap text-sm"
+                        onClick={(el) => {
+                            navigator.clipboard.writeText(
+                                el.currentTarget.textContent
+                            );
+                        }}
+                    >
+                        {GLOBAL_VALUES.phoneNumber}
+                    </p>
+                </u>
+            </div>
+        );
+    }
 
     return (
-        <div
-            className={`${heightCSS} absolute z-10 mt-[1%] flex w-full justify-between bg-purple-700`}
-        >
-            <div className="ml-[4%] mt-[0.15rem]">
-                <Navigation textSize={'text-3xl'} font={'font-jost'} />
-            </div>
+        <div className="flex w-full justify-center">
             <div
-                id="phone-num-and-icons-holder"
-                className="mr-[4%] flex items-center gap-7"
+                className={`${heightCSS} absolute z-10 mt-[1%] flex w-[90%] justify-between`}
             >
-                <div id="phone-number-holder" className="mb-[-3px]">
-                    <u className="phone-number-ul">
-                        <p
-                            className="phone-number-p text-sm"
-                            onClick={(el) => {
-                                navigator.clipboard.writeText(
-                                    el.currentTarget.textContent
-                                );
-                            }}
-                        >
-                            {GLOBAL_VALUES.phoneNumber}
-                        </p>
-                    </u>
+                <div className="mt-[0.15rem]">
+                    <Navigation textSize={'text-3xl'} font={'font-jost'} />
                 </div>
+                <div
+                    id="phone-num-and-icons-holder"
+                    className="flex items-center gap-7 max-laptop:gap-5"
+                >
+                    <PhoneNumber />
 
-                <div id="icons-holder" className={`${heightCSS} flex gap-7`}>
-                    <img
-                        src="images/navbar/telegram.png"
-                        className="icon"
-                        alt="telegram"
-                    />
-                    <img
-                        src="images/navbar/viber.png"
-                        className="icon"
-                        alt="viber"
-                    />
-                    <img src="images/navbar/vk.png" className="icon" alt="vk" />
-                    <img
-                        src="images/navbar/whatsapp.png"
-                        className="icon"
-                        alt="whatsapp"
-                    />
+                    {isBurgerOpened ? (
+                        <FaWindowClose
+                            size={25}
+                            className="hidden text-white max-laptop:block"
+                            onClick={() => setIsBurgerOpened(false)}
+                        />
+                    ) : (
+                        <FaBars
+                            size={25}
+                            className="hidden text-white hover:cursor-pointer max-laptop:block"
+                            onClick={() => setIsBurgerOpened(true)}
+                        />
+                    )}
+                    <SocialMediaIcons heightCSS={heightCSS} />
                 </div>
             </div>
         </div>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        'andika-bold': ['Andika-Bold', 'sans-serif'],
-        jost: ['Jost', 'sans-serif'],
-        'ptsans-bold': ['PTSans-Bold', 'sans-serif'],
-      },
-      screens: {
-        laptop: '1366px',
-        mobile: '650px',
-      },
-      colors: {},
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                'andika-bold': ['Andika-Bold', 'sans-serif'],
+                jost: ['Jost', 'sans-serif'],
+                'ptsans-bold': ['PTSans-Bold', 'sans-serif']
+            },
+            screens: {
+                laptop: '1366px',
+                mobile: '650px'
+            },
+            colors: {}
+        }
     },
-  },
-  plugins: ['prettier-plugin-tailwindcss'],
-}
+    plugins: ['prettier-plugin-tailwindcss']
+};

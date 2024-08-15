4796f9322e9f51c2d5795f10f54cdd12f55ebcc2
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-08-15 12:15:34 +0300

    feat: write benefits + icons for them

--- web/src/components/advantages/Advantages.jsx
+++ web/src/components/advantages/Advantages.jsx
@@ -1,3 +1,33 @@
 export default function Advantages() {
-    return <div className="h-[700px] w-full bg-black"></div>;
+    return (
+        <div className="h-[700px] w-full bg-black">
+            <h1 className="text-white">ВОЗЬМУ ВСЮ РАБОТУ НА СЕБЯ</h1>
+            <p className="text-white">
+                Моя задача - сохранить ваше время и деньги и уберечь от стресса
+                в таком нелегком процессе, как продажа квартиры. Сбор
+                необходимых документов, уборка в квартире, размещение
+                объявлений, показы и заключение сделки — все эти этапы я возьму
+                на себя. А вы каждую неделю будете получать подробный отчет о
+                результатах.
+            </p>
+            <ul className="font-andika-bold text-white">
+                <li>
+                    <img src="images/advantages/benefits.png" alt="" />
+                    <p>Решение о цене недвижимости всегда за клиентом</p>
+                </li>
+                <li>
+                    <img src="images/advantages/pack-house.png" alt="" />
+                    <p>Грамотная упаковка объекта для продажи</p>
+                </li>
+                <li>
+                    <img src="images/advantages/evaluate.png" alt="" />
+                    <p>Точная и качественная оценка квартиры</p>
+                </li>
+                <li>
+                    <img src="images/advantages/adds.png" alt="" />
+                    <p>Эффективная реклама и размещение объявлений</p>
+                </li>
+            </ul>
+        </div>
+    );
 }

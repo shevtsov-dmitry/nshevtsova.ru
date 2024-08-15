89e5b46530cc1a53d7b5a90800cd42e785324500
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-08-15 13:41:27 +0300

    feat: finish with design of this block

--- web/src/components/advantages/Advantages.jsx
+++ web/src/components/advantages/Advantages.jsx
@@ -8,29 +8,29 @@ export default function Advantages() {
           background: "rgb(255,255,255) linear-gradient(180deg, rgba(255,255,255,0.4191877434567577) 15%, rgba(219,216,216,0.6236695361738445) 73%)",
         }}
         >
-            <h1 className=" font-ptsans-bold text-3xl">ВОЗЬМУ ВСЮ РАБОТУ НА СЕБЯ</h1>
-            <p className="">
-                Моя задачать ваше время и деньги и уберечь от стресса
+            <h1 className=" font-ptsans-bold text-4xl">ВОЗЬМУ ВСЮ РАБОТУ НА СЕБЯ</h1>
+            <p className="text-2xl">
+                Моя задача - сохранить ваше время и деньги и уберечь от стресса
                 в таком нелегком процессе, как продажа квартиры. Сбор
                 необходимых документов, уборка в квартире, размещение
                 объявлений, показы и заключение сделки — все эти этапы я возьму
                 на себя. А вы каждую неделю будете получать подробный отчет о
                 результатах.
             </p>
-            <ul className="flex">
-                <li className="flex w-full flex-col items-center justify-center">
+            <ul className="flex text-[1rem]">
+                <li className="advantage-icon-holder">
                     <img src="images/advantages/benefits.png" alt="" />
                     <p>Решение о цене недвижимости всегда за клиентом</p>
                 </li>
-                <li className="flex w-full flex-col items-center justify-center">
+                <li className="advantage-icon-holder">
                     <img src="images/advantages/pack-house.png" alt="" />
                     <p>Грамотная упаковка объекта для продажи</p>
                 </li>
-                <li className="flex w-full flex-col items-center justify-center">
+                <li className="advantage-icon-holder">
                     <img src="images/advantages/evaluate.png" alt="" />
                     <p>Точная и качественная оценка квартиры</p>
                 </li>
-                <li className="flex w-full flex-col items-center justify-center">
+                <li className="advantage-icon-holder">
                     <img src="images/advantages/adds.png" alt="" />
                     <p>Эффективная реклама и размещение объявлений</p>
                 </li>

fd896f10815aba23d8152c0ff0384e805d9dbd77
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-08-15 13:26:15 +0300

    feat: add gradient to block

--- web/src/components/advantages/Advantages.jsx
+++ web/src/components/advantages/Advantages.jsx
@@ -1,29 +1,36 @@
 export default function Advantages() {
     return (
-        <div className="h-[700px] w-full bg-black">
-            <h1 className="text-white">ВОЗЬМУ ВСЮ РАБОТУ НА СЕБЯ</h1>
-            <p className="text-white">
-                Моя задача - сохранить ваше время и деньги и уберечь от стресса
+        <div className="h-fit w-full flex flex-col text-black gap-5 py-[2%] px-[5%]"
+        // style={ {
+        //     background: "rgb(210,210,210) radial-gradient(circle, rgba(210,210,210,0.8253502084427521) 0%, rgba(207,216,226,0.4107843821122199) 100%)",
+        //   } }
+        style={{
+          background: "rgb(255,255,255) linear-gradient(180deg, rgba(255,255,255,0.4191877434567577) 15%, rgba(219,216,216,0.6236695361738445) 73%)",
+        }}
+        >
+            <h1 className=" font-ptsans-bold text-3xl">ВОЗЬМУ ВСЮ РАБОТУ НА СЕБЯ</h1>
+            <p className="">
+                Моя задачать ваше время и деньги и уберечь от стресса
                 в таком нелегком процессе, как продажа квартиры. Сбор
                 необходимых документов, уборка в квартире, размещение
                 объявлений, показы и заключение сделки — все эти этапы я возьму
                 на себя. А вы каждую неделю будете получать подробный отчет о
                 результатах.
             </p>
-            <ul className="font-andika-bold text-white">
-                <li>
+            <ul className="flex">
+                <li className="flex w-full flex-col items-center justify-center">
                     <img src="images/advantages/benefits.png" alt="" />
                     <p>Решение о цене недвижимости всегда за клиентом</p>
                 </li>
-                <li>
+                <li className="flex w-full flex-col items-center justify-center">
                     <img src="images/advantages/pack-house.png" alt="" />
                     <p>Грамотная упаковка объекта для продажи</p>
                 </li>
-                <li>
+                <li className="flex w-full flex-col items-center justify-center">
                     <img src="images/advantages/evaluate.png" alt="" />
                     <p>Точная и качественная оценка квартиры</p>
                 </li>
-                <li>
+                <li className="flex w-full flex-col items-center justify-center">
                     <img src="images/advantages/adds.png" alt="" />
                     <p>Эффективная реклама и размещение объявлений</p>
                 </li>

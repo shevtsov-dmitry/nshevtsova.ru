4638840ed3ab878e29a4bc75421ba090783b42be
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-09-02 14:29:30 +0300

    refactor: test directories  change place

--- server/src/test/java/ru/nshevtsova/controller/EstateControllerTest.java
+++ server/src/test/java/ru/nshevtsova/controller/EstateControllerTest.java
@@ -2,7 +2,6 @@ package ru.nshevtsova.controller;
 
 import org.json.JSONObject;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -33,12 +32,13 @@ public class EstateControllerTest {
     private static Long savedEstateId = 0L;
 
     @RepeatedTest(15)
-//    @Test
+    // @Test
     void saveEstate() throws Exception {
         final var estate = new JSONObject();
 
-//        final String noImageIconBase64 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAnXAAAJ1wGxbhe3AAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAActJREFUOI2dkD1oU2EUhp/33iLFSdHhVprYtLaL0tyquOQKzR0sDgp2s4MuXcSldqyLgyIu16C4qHv9w8WtQyKSQAXBqPiXClUKGkGUYAsdmnscoiGWEEKf5cCB9/m+87p+kC14icGd1ZXlRbaA6yWHdiCue4lUbSsSAfiZ8A5iGuNCuZTPjQbhCQd6Owet8rJYeOUeDLKHkI4BjxGXvUSqZuIXpgOIVcFGe4HO9yUHPvbEaBC4Vy7mH/qZ8AcicmEudmwPxlS8vefw64WFtc0CPwgBjTity3IpnwMeGVwldipCz7RWP9LpFGfzQqLQmHGE2ft11l/4QfgmHYSnuhLs83bdBs0IniCiXrZNG/ZccD+dyU52FIyOj/d/qv68WC4evRnXOWfwXVKE8Ra4K+nBWBCebs24XjK1H2D3wPBvt25PgUkv8WWv47ABnAEkacLM5iV9Bq70JVNLQNwUmKzfNbsFDDWKwEe2CvoGDDc60YSZM4/ir6BLfwUfHACZrjXD/zCdBY7/X3DcPAeYAnCEVQC3XcPtkBSZOUuGbpjZOwGMBdm0oZEuHWlgDmO2XMrn1O3LrfiZcAYRYcx2/fVWqivLi14iVUOc/AMfcZzdCK3KYgAAAABJRU5ErkJggg==";
-//        estate.put("previewImage", noImageIconBase64);
+        // final String noImageIconBase64 =
+        // "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAnXAAAJ1wGxbhe3AAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAActJREFUOI2dkD1oU2EUhp/33iLFSdHhVprYtLaL0tyquOQKzR0sDgp2s4MuXcSldqyLgyIu16C4qHv9w8WtQyKSQAXBqPiXClUKGkGUYAsdmnscoiGWEEKf5cCB9/m+87p+kC14icGd1ZXlRbaA6yWHdiCue4lUbSsSAfiZ8A5iGuNCuZTPjQbhCQd6Owet8rJYeOUeDLKHkI4BjxGXvUSqZuIXpgOIVcFGe4HO9yUHPvbEaBC4Vy7mH/qZ8AcicmEudmwPxlS8vefw64WFtc0CPwgBjTity3IpnwMeGVwldipCz7RWP9LpFGfzQqLQmHGE2ft11l/4QfgmHYSnuhLs83bdBs0IniCiXrZNG/ZccD+dyU52FIyOj/d/qv68WC4evRnXOWfwXVKE8Ra4K+nBWBCebs24XjK1H2D3wPBvt25PgUkv8WWv47ABnAEkacLM5iV9Bq70JVNLQNwUmKzfNbsFDDWKwEe2CvoGDDc60YSZM4/ir6BLfwUfHACZrjXD/zCdBY7/X3DcPAeYAnCEVQC3XcPtkBSZOUuGbpjZOwGMBdm0oZEuHWlgDmO2XMrn1O3LrfiZcAYRYcx2/fVWqivLi14iVUOc/AMfcZzdCK3KYgAAAABJRU5ErkJggg==";
+        // estate.put("previewImage", noImageIconBase64);
         estate.put("price", rand.nextInt(900_000, 20_000_000));
         estate.put("address", randomizeString());
         estate.put("estateType", rand.nextDouble() > 0.5 ? "APARTMENT" : "HOUSE");
@@ -65,8 +65,8 @@ public class EstateControllerTest {
 
         final String url = ENDPOINT_URL + "/estates/add";
         mockMvc.perform(post(url)
-                        .content(String.valueOf(requestJsonData))
-                        .contentType("application/json"))
+                .content(String.valueOf(requestJsonData))
+                .contentType("application/json"))
                 .andExpect(status().is(200))
                 .andDo(result -> System.out.println(result.getResponse().getContentAsString()));
     }
@@ -78,6 +78,5 @@ public class EstateControllerTest {
         }
         return sb.toString();
     }
-
 
 }
---
+++ server/src/test/java/ru/nshevtsova/custom/records/NameSurname.java
@@ -0,0 +1,7 @@
+package ru.nshevtsova.custom.records;
+
+/**
+ * NameSurname
+ */
+public record NameSurname(String name, String surname) {
+}
